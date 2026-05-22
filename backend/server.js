490eba309e8c83808049280a25c667b043406df1
Saif Alam <mdsaifalam701@gmail.com>
2026-05-22 16:46:01 +0530

    Add Loader.io verification route

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,16 @@ connectDB();
 // Accessing port from .env file
 const PORT = process.env.PORT || 5000;
 
+app.get('/loaderio-e3f29f018776f5a088f65fc56211fb5f.txt', (req, res) => {
+    res.send('loaderio-e3f29f018776f5a088f65fc56211fb5f');
+});
+
 app.use("/api", userRouter);
 app.use("/api", quizRouter);
 
 // Socket connection
 socketHandler(io)
 
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 })
