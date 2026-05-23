75f7c67bb343f6dc2bb7f9019b8801c4b6979a15
Saif Alam <mdsaifalam701@gmail.com>
2026-05-23 22:06:00 +0530

    add no-chache

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from "express";
-import userRouter from "./routes/useRoute.js"
-import quizRouter from "./routes/quizRoute.js"
+import userRouter from "./routes/useRoute.js";
+import quizRouter from "./routes/quizRoute.js";
 import connectDB from "./config/db.js";
 import { configDotenv } from "dotenv";
 import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
-import socketHandler from "./sockets/socketHandler.js"
+import socketHandler from "./sockets/socketHandler.js";
 
 configDotenv();
 
@@ -18,9 +18,9 @@ const server = http.createServer(app);
 
 // Attach Socket.IO
 const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
 app.use(express.json());
@@ -31,16 +31,23 @@ connectDB();
 // Accessing port from .env file
 const PORT = process.env.PORT || 5000;
 
-app.get('/loaderio-e3f29f018776f5a088f65fc56211fb5f.txt', (req, res) => {
-    res.send('loaderio-e3f29f018776f5a088f65fc56211fb5f');
+app.use("/api", (req, res, next) => {
+  res.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate",
+  );
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+
+  next();
 });
 
 app.use("/api", userRouter);
 app.use("/api", quizRouter);
 
 // Socket connection
-socketHandler(io)
+socketHandler(io);
 
 server.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
+  console.log(`server is running on port ${PORT}`);
+});
